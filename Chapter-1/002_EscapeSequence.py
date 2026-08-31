a2780c478e2a9ae27b232dea577e5b48480d0cdf
Chirag Kukreja <chirag.kukreja24@gmail.com>
2026-08-31 23:02:02 +0530

    Escape Sequences

---
+++ Chapter-1/002_EscapeSequence.py
@@ -0,0 +1,20 @@
+# Escape Sequences
+# \' - Single Quote
+# \" - Double Quote
+# \\ - Backslash
+# \n - New Line
+# \t - Tab
+# \b - Backspace
+
+# Printing a string with escaped double quotes
+print ("Hello \"World!\" Dunia")
+# Printing a string with escaped single quote
+print ('I\'m learning Python programming.')
+# Printing a string with escaped backslash
+print ("This is a string with a backslash: \\")
+# Printing a string with escaped new line character
+print ("This is a string with a new line:\nHello!")
+# Printing a string with escaped tab character
+print ("This is a string with a tab:\tHello!")
+# Printing a string with escaped backspace character
+print ("This is a string with a backspace: Hello\bWorld!")
