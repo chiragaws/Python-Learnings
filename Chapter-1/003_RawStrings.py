b20d1faccd73d67b71cfb9b3c294ee3603819e72
Chirag Kukreja <chirag.kukreja24@gmail.com>
2026-09-06 13:56:17 +0530

    Raw Strings

---
+++ Chapter-1/003_RawStrings.py
@@ -0,0 +1,4 @@
+# The r prefix makes this a raw string, so backslashes are treated literally
+# and are NOT needed to escape the double quotes here (though escaping still
+# works inside a raw string when quoting the same delimiter).
+print(r"Hello \"World!\" Dunia")
