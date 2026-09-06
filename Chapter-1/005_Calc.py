9a4e8851f2bed7c9063f4e947b8ee87b33176254
Chirag Kukreja <chirag.kukreja24@gmail.com>
2026-09-06 14:54:29 +0530

    Bodmas and Calc

--- Chapter-1/005_Calc.py
+++ Chapter-1/005_Calc.py
@@ -3,3 +3,11 @@ print(2+3*5)
 print(10/3)
 #Integer Division
 print(10//3)
+#Exponential
+print(2**3)
+#Root
+print(2**0.5)
+#Round-off
+print(round(2.675635634654654, 2)) #Output: 2.68
+#BODMAS
+print(2**3/2*6-4*(3-4/2)) #Output: 20
