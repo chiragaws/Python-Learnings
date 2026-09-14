d41af937eefb28ad9dd32029f2f0cecfdeb0a6e3
Chirag Kukreja <chirag.kukreja24@gmail.com>
2026-09-14 22:00:12 +0530

    Added More Rules and Dynamic Programming Stuff.

--- Chapter-1/006_Variables.py
+++ Chapter-1/006_Variables.py
@@ -1,4 +1,37 @@
+# This file demonstrates basic variable assignment and printing in Python
+
+# Initialize a variable named 'number1' with the value 2
 number1 = 2
+
+# Print the current value of 'number1' (which is 2)
 print(number1)
+
+# Update the value of 'number1' to 3
 number1 = 3
+
+# Print the updated value of 'number1' (now 3)
 print(number1)
+
+name  = "Chirag Kukreja"    
+print (name)  # Print the value of the variable 'name' (which is "Chirag Kukreja")
+name = 121
+print (name)  # Print the updated value of 'name' (now 121) # This is called Dynamic Programming in Python, where the type of a variable can change during execution.
+
+#Rules for naming variables in Python:
+# 1. Variable names can contain letters, numbers, and underscores (_).
+# 2. Variable names must start with a letter or an underscore (_).  
+# 3. Variable names are case-sensitive (e.g., 'myVar' and 'myvar' are different variables).
+# 4. Variable names cannot be a reserved keyword in Python (e.g., 'if', 'for', 'while', etc.). # This is a Dynamic Programming problem.
+# 5. Variable names should be descriptive and meaningful to improve code readability.
+# 6.  You can not name 1name as a variable name. It should start with a letter or underscore.
+
+# Naming Conventions for naming variables in Python:
+# 1. Use lowercase letters for variable names (e.g., 'my_variable').
+# 2. Use underscores to separate words in variable names (e.g., 'my_variable_name').
+# 3. Avoid using single-character variable names, except for loop counters or temporary variables.
+#Snake Case: my_variable_name
+user_name_one="Chirag Kukreja"
+#CAMEL Case: userNameOne
+userNameOne="Chirag"
+# 4. Variable names must start with a letter or an underscore. Single-character variable names are allowed, but they should be used sparingly and only for temporary or loop variables.
+# Use Snake Case for variable names in Python, which means using lowercase letters and underscores to separate words (e.g., 'my_variable_name').
